636665e8ddd11acfe9d4957fd8f8ddc59b2d3121
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-06-16 20:12:40 +0600

    css design part st

--- js/mian.js
+++ js/mian.js
@@ -26,37 +26,24 @@ let handlebutton = () => {
         fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input.value}&limit=2&appid=f921b0d5d82c2356163bcab823b1b23f`)
 
             .then(res => res.json())
-            .then((data)=>{
-               
-
-
-       let myarrray=       fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=f921b0d5d82c2356163bcab823b1b23f`)
-
-.then( res => res.json())
-.then((data)=>{
+            .then((data) => {
 
 
 
+                let myarrray = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=f921b0d5d82c2356163bcab823b1b23f`)
+
+                    .then(res => res.json())
+                    .then((data) => {
 
 
+                        let result = Math.floor(data.main.temp - 273)
+                        temp.innerHTML = result + "°C";
 
 
-let xxx=Math.floor(data.main.temp - 273)
-temp.innerHTML=xxx +"°C";
+                        cardHED.innerHTML = input.value
 
-
-cardHED.innerHTML=input.value
-
-
-
-
-     console.log(data.main.temp - 273.15 )
-})
-
-
-
-
-
+                        console.log(data.main.temp - 273.15)
+                    })
 
             })
 
@@ -70,25 +57,3 @@ cardHED.innerHTML=input.value
 
 
 }
-
-function updateClock() {
-  let now = new Date();
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  let seconds = now.getSeconds();
-
-  // যদি ঘন্টা, মিনিট বা সেকেন্ড এক সংখ্যার হয় (০-৯), তাহলে তার আগে '0' বসাই
-  hours = hours < 10 ? "0" + hours : hours;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
-
-  let timeString = hours + ":" + minutes + ":" + seconds;
-
-  document.getElementById("clock").innerHTML = timeString;
-}
-
-// প্রতি ১ সেকেন্ড (1000 milliseconds) পরপর updateClock() চালাও
-setInterval(updateClock, 1000);
-
-// প্রথমবার চালাও যাতে দেরি না হয়
-updateClock();

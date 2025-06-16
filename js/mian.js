fa368aa2c2f5d3ee4f8ac3d71dcf11edae20def7
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-06-16 16:37:08 +0600

    dom selcet

---
+++ js/mian.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+let h2 = document.querySelector('h2')
+let input = document.querySelector('input')
+let button = document.querySelector('button')
+
+
+
+// let h2 = document.querySelector('h2')
+// let h2 = document.querySelector('h2')
+// let h2 = document.querySelector('h2')
+// let h2 = document.querySelector('h2')
+
+
